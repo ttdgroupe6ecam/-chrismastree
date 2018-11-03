6687e4bd2e0114ba8202ed5aaa5b32dcd41e1696
unknown <yass.d@outlook.com>
2018-11-03 20:05:06 +0100

    update ArtificialTree class

--- src/christmasTree/ArtificialTree.java
+++ src/christmasTree/ArtificialTree.java
@@ -14,14 +14,17 @@ public class ArtificialTree extends ChrismasTree {
     private  String  plasticType;
     private  boolean hasRoot;
     
+    //constructs
     public ArtificialTree() {
         setDescription("Artificial Tree");
         setPrice(100.99);
     }
     
-    public ArtificialTree( String description,  double price,  String woodType,  boolean hasRoot){
+    public ArtificialTree( String description,  double price,  String plasticType,  boolean hasRoot){
         setDescription(description);
         setPrice(price);
+        this.plasticType=plasticType;
+        this.hasRoot=hasRoot;
         
     }
     
