3413776a7c87a370c474e328d9fad4bf296d8d3f
venon arthur <venon arthur@192.168.0.100>
2018-11-06 15:10:02 +0100

    the FileWriter class is used to save a bill, by having append to true we ensure that writing onto that file will had it to it's end so we can make it in multiple time

---
+++ src/christmasTree/FileWriter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package christmasTree;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author venon arthur
+ */
+public class FileWriter {
+    private String path;
+    private boolean append = true;
+    
+    public void WriteFile (String file_path) {
+        path = file_path;
+                }
+    
+}
