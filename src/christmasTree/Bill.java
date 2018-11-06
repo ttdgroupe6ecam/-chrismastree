dfa7fca46931dfea9cc962169afedbb6ff0512e7
venon arthur <venon arthur@192.168.0.100>
2018-11-06 14:53:01 +0100

    useful imports for the text file system

--- src/christmasTree/Bill.java
+++ src/christmasTree/Bill.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 package christmasTree;
-
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 /**
  *
  * @author venon arthur
  */
 public class Bill {
+    
     
 }
