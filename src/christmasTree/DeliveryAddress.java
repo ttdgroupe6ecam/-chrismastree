8802f9d2faf56ff638797d5b94eaec196be7dbd0
Mohamad <15107@ecam.be>
2018-11-07 13:16:21 +0000

    add

---
+++ src/christmasTree/DeliveryAddress.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package christmasTree;
+
+/**
+ *
+ * @author mohamad
+ */
+public class DeliveryAddress {
+    String DeliveryAddress;
+    
+    public void setDeliveryAddress(String DeliveryAddress) {
+        this.DeliveryAddress = DeliveryAddress;
+    }
+    
+        public String getDeliveryAddress() {
+        return DeliveryAddress;
+    }
+}
