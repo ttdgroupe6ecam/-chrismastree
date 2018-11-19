e6f29fe745f5e41bb8f2ec057cf95bacdba44a12
Xcrid <venonarthur.venon4@gmail.com>
2018-11-19 18:02:00 +0100

    add balls test

--- src/test/BallsTest.java
+++ src/test/BallsTest.java
@@ -1,19 +1,20 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-import christmastree.ChrismasTree;
+import christmastree.NaturalTree;
 import christmastree.Balls;
 
 public class BallsTest {
 
-    private static ChrismasTree tree;
-    private static Balls bal1=new Balls(tree);
+    private static NaturalTree tree;
+    private static Balls bal1;
 
 
     @BeforeClass
     public static void setupBeforeClass() {
 
 
+        ball = new Balls(tree);
     }
 
     @Test
