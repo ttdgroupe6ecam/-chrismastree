746f7846c3a90ca5672daa9804cc5066ea2ea67b
Xcrid <venonarthur.venon4@gmail.com>
2018-11-19 15:54:59 +0100

    Creation of a test class for the order one

--- src/test/OrderTest.java
+++ src/test/OrderTest.java
@@ -1,30 +1,30 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-import Order;
+import main.Order;
 
 public class OrderTest {
 
     private static Order order;
 
     @BeforeClass
-    public static setupBeforeClass() {
+    public static void setupBeforeClass() {
 
         order =new Order("31 rue de Keersmaeker","Arthur","Venon");
     }
 
     @Test
-    public getDeliveryAddress() {
+    public void getDeliveryAddress() {
         assertEquals(order.getDeliverryAddress(),"31 rue de Keersmaeker");
     }
 
     @Test
-    public getName() {
+    public void getName() {
         assertEquals(order.getName(),"Arthur");
     }
 
     @Test
-    public getSurname() {
+    public void getSurname() {
         assertEquals(order.getSurname(),"Venon");
     }
 
@@ -32,5 +32,3 @@ public class OrderTest {
 
 
 
-
-}
