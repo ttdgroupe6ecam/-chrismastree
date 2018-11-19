faa0d965811b2d15f377a2a190102a103c729985
Xcrid <venonarthur.venon4@gmail.com>
2018-11-19 16:56:56 +0100

    add chrismastree test

---
+++ src/test/ChristmasteeTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import christmastree.ChrismasTree;
+
+public class ChristmastreeTest {
+
+    private static ChrismasTree tree;
+
+
+    @Test
+    public void testDescription() {
+        tree.setDescritpion("test");
+        assertEquals(tree.getDescritpion(),"test");
+    }
+
+    @Test
+    public void testName() {
+        tree.setPrice(15.3);
+        assertEquals(tree.getPrice(),15.3);
+    }
+
+    @Test
+    public void testSurname() {
+        tree.setColor("rouge");
+        assertEquals(tree.getColor(),"rouge");
+    }
+
+}
