8ba441cc3c4094dfd3ed28ce861d87fc701f3a4b
Xcrid <venonarthur.venon4@gmail.com>
2018-11-19 17:31:31 +0100

    add artificial tree

---
+++ src/test/ArtificialTreeTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import christmastree.ArtificialTree;
+
+public class ArtificialTreeTest {
+
+    private static ArtificialTree tree1;
+    private static ArtificialTree tree2;
+
+    @BeforeClass
+    public static void setupBeforeClass() {
+
+
+        tree2 = new ArtificialTree("artificiel",23.8,"jaune","bisphenol A");
+    }
+
+    @Test
+    public void constructStandard() {
+        tree1 =new ArtificialTree();
+        assertEquals(tree1.getPlasticType(),"plasticType: bisphenol A");
+    }
+
+
+
+    @Test
+    public void getToString() {
+        assertEquals(tree2.toString()," ArtificialTree{plasticType: bisphenol A}");
+    }
+
+}
