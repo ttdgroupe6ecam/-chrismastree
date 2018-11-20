3c514aaefc3ca816f170f4762a8d8609c3a38ee0
Xcrid <36483768+Xcrid@users.noreply.github.com>
2018-11-20 09:39:11 +0100

    Update NaturalTreeTest.java

--- src/test/NaturalTreeTest.java
+++ src/test/NaturalTreeTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-import main.NaturalTree;
+import christmastree.NaturalTree;
 
 public class NaturalTreeTest {
 
