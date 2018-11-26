83ad9eff875a54509b41afa58ea9ff802e3cdb64
Xcrid <venonarthur.venon4@gmail.com>
2018-11-26 13:37:36 +0100

    add balls junit testing

---
+++ src/test/BallsTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import christmastree.ArtificialTree;
+
+public class BallsTest {
+
+    private static ArtificialTree tree1;
+    private static Balls ball;
+
+    @BeforeClass
+    public static void setupBeforeClass() {
+
+
+        tree2 = new ArtificialTree("artificiel",23.8,"jaune","bisphenol A");
+        ball = new Balls(tree2);
+    }
+
+    @Test
+    public void getSize() {
+
+    assertEquals(ball.getSize(), " BallsSize: Small");
+    }
+
+
+
+    @Test
+    public void getToString() {
+        //assertEquals(tree2.toString()," ArtificialTree{plasticType: bisphenol A}");
+    }
+
+}
